3a516777e87ccfe87fd520a92cecc433537a3e23
Sergei Pofalitov <s.pofalitov@corp.mail.ru>
2023-06-18 00:13:54 +0300

    create from

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React, { ChangeEventHandler, MouseEventHandler, useState } from "react";
+
+interface FormRowProps {
+  children: React.ReactNode;
+}
+
+const FormRow: React.FC<FormRowProps> = ({ children }) => {
+  return <div className="form-row">{children}</div>;
+};
+
+const createRow = (name?: string, surname?: string) => ({
+  name: name ?? "",
+  surname: surname ?? "",
+});
+
+const defaultValue = [createRow()];
 
 function App() {
+  const [state, setState] = useState(defaultValue);
+
+  const handleSubmit = () => {
+    console.log(state);
+  };
+
+  const handleAddRow: MouseEventHandler<HTMLButtonElement> = (event) => {
+    console.log(event);
+  };
+
+  const handleDeleteRow: MouseEventHandler<HTMLButtonElement> = (event) => {
+    console.log(event);
+  };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(event.target.name, event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <form className="form" noValidate autoComplete="off">
+        <Button onClick={handleSubmit}>Submit</Button>
+
+        {state.map((item, i) => (
+          <FormRow key={i}>
+            <TextField
+              fullWidth
+              name="name"
+              label="Name"
+              variant="outlined"
+              value={item.name}
+              onChange={handleChange}
+            />
+            <TextField
+              fullWidth
+              name="surname"
+              label="Surname"
+              value={item.surname}
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <div className="form-row__buttons">
+              <Button onClick={handleAddRow}>Add</Button>
+              <Button onClick={handleDeleteRow}>Delete</Button>
+            </div>
+          </FormRow>
+        ))}
+      </form>
     </div>
   );
 }
